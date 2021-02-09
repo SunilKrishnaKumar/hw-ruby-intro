3f32db1990cd36b4d85bcafb477501c1cfb09a59
EC2 Default User <ec2-user@ip-172-31-2-104.us-east-2.compute.internal>
2021-02-09 04:59:09 +0000

    part1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,27 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  else
+    s = 0
+    arr.each do |i|
+      s += i
+    end
+    return s# puts s
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr.first if arr.length == 1
+
+  arr.sort{ |x, y| y <=> x }.take(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty? && n.zero?
+  arr.combination(2).any? {|a, b| a + b == n }
 end
 
 # Part 2
